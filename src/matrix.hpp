adf47f5467539d50f85cb3a7ca111d12e78e52d9
Masatoshi Takayanagi <handmcmasa@gmail.com>
2023-08-18 09:54:10 +0900

    テンプレート化
    float
    double
    complex<float>
    complex<double>

--- src/matrix.hpp
+++ src/matrix.hpp
@@ -4,10 +4,11 @@
 #include <iostream>
 #include <cstdint>
 
+template <typename T>
 class Matrix {
     
 protected:
-    double *top_;     // pointer to the matrix
+    T *top_;     // pointer to the matrix
     uint32_t m_;  // number of lows of the matrix or lda
     uint32_t n_;  // number of columns of the matrix
     uint32_t mb_{}; // number of lows of the tile
@@ -23,24 +24,24 @@ public:
     // Constructor
     Matrix(const uint32_t &m, const uint32_t &n):m_{m}, n_{n},
                                                  mb_{m}, nb_{n},
-                                                 p_{1}, q_{1}, top_{new double[m*n]} {};
+                                                 p_{1}, q_{1}, top_{new T[m*n]} {};
     Matrix(const uint32_t &m, const uint32_t &n, const uint32_t &ts):m_{m}, n_{n},
                                                                      mb_{ts}, nb_{ts},
                                                                      p_{(m % ts == 0) ? m / ts : m / ts + 1},
                                                                      q_{(n % ts == 0) ? n / ts : n / ts + 1},
-                                                                     top_{new double[m*n]} {};
+                                                                     top_{new T[m*n]} {};
     Matrix(const uint32_t &m, const uint32_t &n,
            const uint32_t &mb, const uint32_t &nb):m_{m}, n_{n},
                                                    mb_{mb}, nb_{nb},
                                                    p_{(m % mb == 0) ? m / mb : m / mb + 1},
                                                    q_{(n % nb == 0) ? n / nb : n / nb + 1},
-                                                   top_{new double[m*n]} {};
+                                                   top_{new T[m*n]} {};
 
     // Destructor
     ~Matrix();
     
     // Getters
-    double *top() { return top_; }
+    T *top() { return top_; }
     uint32_t m() const { return m_; }
     uint32_t n() const { return n_; }
     uint32_t mb() const { return mb_; }
@@ -57,27 +58,22 @@ public:
     // Assign random numbers to matrix elements
     void gen_rnd_elm();
     // the pointer of the (i,j) element
-    double *elm(const uint32_t &ti, const uint32_t &tj) const;
+    T *elm(const uint32_t &ti, const uint32_t &tj) const;
     // the pointer of the (i,j) element (ti,tj)
-    double *elm(const uint32_t &ti, const uint32_t &tj,
-                const uint32_t &i, const uint32_t &j) const;
+    T *elm(const uint32_t &ti, const uint32_t &tj,
+           const uint32_t &i, const uint32_t &j) const;
     
     void file_out(const char *fname);
 
     void zero();
     
     // Operator overload
-    Matrix &operator=(const Matrix &T);
-    Matrix operator+(const Matrix &T) const;
-    Matrix operator-(const Matrix &T) const;
-    bool operator==(const Matrix &T);
-    double &operator[](const uint64_t &i) const;
-    double &operator()(const uint32_t &i, const uint32_t &j) const;
-    
-    /*
-      template<typename CharT, typename Traits>
-      std::basic_ostream<CharT, Traits>&
-      operator<<(std::basic_ostream<CharT, Traits>& os, const Matrix& ma);
-    */
+    Matrix &operator=(const Matrix &M);
+    Matrix operator+(const Matrix &M) const;
+    Matrix operator-(const Matrix &M) const;
+    bool operator==(const Matrix &M);
+    T &operator[](const uint64_t &i) const;
+    T &operator()(const uint32_t &i, const uint32_t &j) const;
+
     friend std::ostream &operator<<(std::ostream &os, const Matrix &ma);
 };
